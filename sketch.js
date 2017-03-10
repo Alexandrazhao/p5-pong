13a1bc479ad3b0545f87b93da02f1eaa53f1573c
Alexandrazhao <yz3514@bard.edu>
2017-03-09 23:59:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
 var img1;
 var img2;
+var img3;
+var img4;
+var img5;
+var img6;
+var img7;
+var img8;
+
 var x = 100;
 var y = 0;
 var face;
@@ -12,16 +19,21 @@ var hit=false;
 
 
 function preload(){
-    img1=loadImage("G_2.png");
-    img2=loadImage("G_3.png")
-    
+    img1=loadImage("New_P.png");
+    img2=loadImage("egg_yolk.png");
+    img3=loadImage("New_P.png");
+    img4=loadImage("black_heart.png");
+    img5=loadImage("black_heart.png");
+    img6=loadImage("black_heart.png");
+    img7=loadImage("New_P.png");
+    img8=loadImage("big_heart.png");
 }
 function setup() {
     createCanvas(1000,500); 
-   
-   
+    
     strokeWeight(0);
-    frameRate(20);
+    frameRate(5);
+    
     
     
     
@@ -31,13 +43,7 @@ function setup() {
 function draw() {
    background(0);  
     
-    
-  image(img1,20,450);
-  image(img1,40,450);
-    image(img1,60,450);
-    image(img1,80,450);
-   
-    fill(random(0,255),random(150,200),random(0,255))
+    fill(random(0,255),random(100,200),random(100,200))
     rect(0,300,100,20);
     rect(200,random(200,370),60,20);
     rect(310,random(80,150),70,20);
@@ -53,50 +59,119 @@ function draw() {
     face(50,50);
     
     hit1 = collideRectCircle(200,random(200,370),60,20,x,y+280,40,50);
-   
+    print ("I will give you one more chance!" + hit1);
     if (hit1){
-            alert("you die!");
+        image (img1,390,400);  
+         image (img8,400,100);  
+       
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        
+    } 
+    else if (true){
+        image (img4,390,400);
     }
+    
+    
+    
     hit2 = collideRectCircle(310,random(80,150),70,20,x,y+280,40,50);
     if (hit2){
-        alert("you are loser");
+        image (img3,340,400);
+        image(img8,400,100);
+        
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+    }
+    else if(true){
+        image (img5,340,400);
+        
     }
     
     hit3 = collideRectCircle(450,random(360,480),40,20,x,y+280,40,50);
     if (hit3){
-        alert("you have already passed two sessions! Owwww!")
+        image (img7,290,400);
+        image (img8,400,100);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+    }
+     else if(true){
+        image (img6,290,400);
     }
     hit4 = collideRectCircle(500,random(200,400),90,20,x,y+280,40,50);
     if (hit4){
-        alert("you are kind of good, but...")
+        alert("you are kind of good, but...");
+        image(img2,300,100,400,200);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        reset();
     }
+     
     hit5 = collideRectCircle (600,random(50,150),20,20,x,y+280,40,50);
     if (hit5){
-        alert("half way died")
+        alert("half way died");
+        image(img2,300,100,400,200);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        reset();
     }
      hit6 = collideRectCircle(630,random(20,400),50,20,x,y+280,40,50);
     if (hit6){
-        alert("Good Job, why not keep further?")
+        alert("Good Job, why not keep further?");
+        image(img2,300,100,400,200);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        reset();
     }
     hit7 =collideRectCircle(700,random(300,500),90,20,x,y+280,40,50);
     if (hit7){
-        alert("AHA!")
+        alert("AHA!");
+        image(img2,300,100,400,200);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        reset();
     }
     hit8 =collideRectCircle(800,random(50,380),30,20,x,y+280,40,50);
     if (hit8){
-        alert("No way!!")
+        alert("No way!!");
+        image(img2,300,100,400,200);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        reset();
     }
     hit9 =collideRectCircle(820,random(160,270),40,20,x,y+280,40,50);
     if (hit9){
-        alert("you are almost win this game!")
+        alert("you are almost win this game!");
+        image(img2,300,100,400,200);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        reset();
     }
     hit10 =collideRectCircle(910,random(310,450),30,20,x,y+280,40,50);
     if (hit10){
-        alert("Okay, i have noting to day baby.")
+        alert("Okay, i have noting to day baby.");
+        image(img2,300,100,400,200);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        reset();
     }
     hit11 =collideRectCircle(950,random(200,400),50,20,x,y+280,40,50);
     if (hit11){
-        alert("Dude! What are you doing!")
+        alert("Dude! What are you doing!");
+        image(img2,300,100,400,200);
+        fill(255,0,0);
+        ellipse(x+15/4,y+270,5,5);
+        ellipse(x+25/2,y+270.5,5);
+        reset();
     }
     
      
@@ -106,13 +181,14 @@ function draw() {
 
   
 function face(){
+    
      
      if(keyIsPressed){
         if(keyCode==LEFT_ARROW){
-            x--; 
+            x = x-6; 
         }
          else if (keyCode == RIGHT_ARROW){
-            x++;
+            x = x+6;
          }
          else {loop
               }    
@@ -120,27 +196,28 @@ function face(){
     }
     if(keyIsPressed){
         if((key=='w')||(key=='W')){
-            y--;
+            y = y-6;
         }
         else if ((key=='s')||(key=='S')){
-            y++;
+            y = y+6;
         }
         else{loop}
+    
         
     }
+     
       
-     fill(248,224,125);
+     fill(255,random(100,200),random(20,50));
     ellipse(x,y+280,40,50);
+    
     
      fill(0);
     ellipse(x+15/4,y+270,5,5);
     ellipse(x+25/2,y+270.5,5);
-         
-      
     
     triangle(x+random(10,18),y+random(270,285),x+random(23,30),y+random(275,285),x+random(10,30),y+random(275,290));
     
-          
+      
   
     
 }
